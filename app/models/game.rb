d1d76a4f691c574ec480e697e540eedb35985b07
danielglatstein <danielglatstein@gmail.com>
2015-10-08 17:28:09 -0400

    add outline to game class

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,38 @@
 require 'pry'
 
 class Game
-  
+
+  def reject_letter
+    #take wrong input and add it to the wrong array
+  end
+
+  def remove_letter
+    #this method takes the selected letter and removes from the letters.all
+  end
+
+  def accept_letter
+    #this method takes the selected letter and adds it to the hangman
+  end
+
+  def word_contains?
+    #returns true/false value if words contains? letter
+  end
+
+  def valid?
+    #is the users input a valid, unused letter
+  end
+
+  def order(letter)
+    letter_input = gets.chomp
+    valid?
+    if false
+      reject_letter
+    end
+    word_contains?
+    if true
+      accept_letter
+    elsif false
+      remove_letter
+    end
+  end
 end
