0528088c48a4fd22af8dc5d5cd6427457effc734
danielglatstein <danielglatstein@gmail.com>
2015-10-08 22:09:49 -0400

    start refactoring board.reveal

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,11 +3,12 @@ require 'pry'
 class Board
   #this is gonna be the picture
   #it will have to update after every input
-  attr_reader :word, :letters
+  attr_reader :word, :letters, :blanks
   
   def initialize
-    @word = Word.answer
+    @word = Word.answer.split(' ')
     @letters = Letters.new
+    @blanks = Array.new(Word.answer.length, "_")
   end
 
   def wrong_guess(letter)
@@ -18,27 +19,39 @@ class Board
     end
   end
 
-  def blanks
-    Array.new(word.length, "_")
-  end
-
-  def accept_letter
+  def accept_letter(letter)
     #this method takes the selected letter and adds it to the hangman
-    if valid? && word.answer.include?(letter)
-      
+    if valid? && word.include?(letter)
+      reveal
     end
   end
 
-  def replace_blank
-
+  def reveal(letter)
+    correct = []
+    count = 0
+    word.each do |letter_index|
+      correct << count if letter_index == letter
+      count += 1
+    end
+    correct.each do |i|
+      blanks[i] = letter
+    end
+    blanks
   end
-  
+
   def valid?(letter)
-    if the letter.length = 1 && !letters.wrong.include?(letter)
+    if the letter.length == 1 && !letters.wrong.include?(letter) && !letters.correct.include?(letter)
       return true
     else
       return false
     end
   end
 
+  def win
+    blanks == word
+  end
+
+  def loss
+    letters.wrong.length == 5
+  end
 end
