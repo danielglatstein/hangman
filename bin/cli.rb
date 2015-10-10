1e777e494168c7fb1ed5b85ec83df0835a2ec028
danielglatstein <danielglatstein@gmail.com>
2015-10-10 12:52:24 -0400

    add while loop to cli.rb in order to loop through guessing letter

--- bin/cli.rb
+++ bin/cli.rb
@@ -16,33 +16,36 @@ while x != 'quit'
   when 'play'
     controller.guess_prompt
     action = gets.chomp
-    if action == 'letter'
-      #how do we keep it in this loop until
-      #the game is over or the player
-      #decides to try to answer
-      controller.letter_prompt
-      guess = gets.chomp
-        if !game.valid(guess)
-          controller.invalid_error("letter")  
-        elsif game.loss
-          controller.game_over(word)
-        elsif game.win
-          controller.win_message
-        else
-          game.round(guess)
-          puts game.blanks
-        end
-    #binding.pry
-    elsif action == 'answer'
-      controller.answer_prompt
-      guess = gets.chomp
-        if guess == word.join
-          controller.win_message
-        else game.loss
-          controller.game_over(word)
-        end
-    else
-      controller.guess_prompt
+    while !game.loss && !game.won
+      if action == 'letter'
+        #how do we keep it in this loop until
+        #the game is over or the player
+        #decides to try to answer-->trying to put
+        #in while loop
+        controller.letter_prompt
+        guess = gets.chomp
+          if !game.valid(guess)
+            controller.invalid_error("letter")  
+          elsif game.loss
+            controller.game_over(word)
+          elsif game.win
+            controller.win_message
+          else
+            game.round(guess)
+            puts game.blanks
+          end
+      #binding.pry
+      elsif action == 'answer'
+        controller.answer_prompt
+        guess = gets.chomp
+          if guess == word.join
+            controller.win_message
+          else game.loss
+            controller.game_over(word)
+          end
+      else
+        controller.guess_prompt
+      end
     end
   end
 end 
